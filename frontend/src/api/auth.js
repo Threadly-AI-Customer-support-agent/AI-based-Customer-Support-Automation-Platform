9abffd00634f42950610df22f7832e7ba440d53b
khushi rokde <khushirokdejee@gmail.com>
2026-03-16 13:13:47 +0530

    removed frontend folder

--- frontend/src/api/auth.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = axios.create({ baseURL: "http://localhost:5000/api/auth" });
-
-export const login = (data) => API.post('/login', data); // Hits backend login route
-export const register = (data) => API.post('/register', data); // Hits backend register route
